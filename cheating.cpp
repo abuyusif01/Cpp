2f0176c5d22e4b39d22dbb9693c87242bce3c56f
Abubakar Yusif <abubakaryusuf951@gmail.com>
2019-10-26 17:33:14 +0800

    Add user input

--- cheating.cpp
+++ cheating.cpp
@@ -19,6 +19,7 @@ void find_mean(int list[], int sizeofarray){
 }
 int main(){ 
     int size;
+    cout<<"Enter the size of an array: ";
     cin>>size;
     int array[size];
     for (int i =0; i < size; i++){
