a931b678df15a3e93d48f9dc99c31aa84f413d32
blackdracula <Abubakaryusuf951@gmail.com>
2019-11-23 20:09:14 +0800

    am nearly died

--- ICO/lib/shift.h
+++ ICO/lib/shift.h
@@ -1,23 +1,49 @@
 #include <bits/stdc++.h>
-#include "shift.h"
+#include "assign.h"
 #pragma once
-std::vector<char> __Q__,__M__,__A__,__Q_1__;
-std::vector<char> __assign_all__;
-void assign_all(std::vector<char> Q, std::vector<char> A){
-    std::vector<char> local;
 
-    __Q__ = Q; /*ok it start becoming confusing lol,*/
-    __A__ = A; /*literally assigning the global to local,*/
+/*so now we need a reference to main
+values, thats the point of whole this
+reassigning values*/
 
-    for (int i=0; i<(int)(A.size()); i++){
-        local.push_back(Q.at(i));
+std::vector<char>& __m__ = __M__;
+std::vector<char>& __a__ = __A__;
+std::vector<char>& __q__ = __Q__;
+std::vector<char>& __q_1 = __Q_1__;
+void shift___q_1(){
+    __q_1.push_back(__assign_all__.at(__assign_all__.size()-1));
+    std::cout<<"value of __q_1 in the function: "<<__q_1.at(0);
+}
+void shift___m__(){}// this shit is constant.
+void shift___q__(std::vector<char> &__q__){
+    __q__.clear();
+    for(int i=(int)(__assign_all__.size()-1); i>=4; --i){
+        __q__.push_back(__assign_all__.at(i-1));
+        std::cout<<"fuck at: "<<i<<"\n";
     }
-    for (int j=0; j<(int)(A.size()); j++){
-        local.push_back(A.at(j));
-    }
-    __assign_all__ = local;
-    for (int k =0; k<(int)(__assign_all__.size()); k++){
-    std::cout<<__assign_all__.at(k);
+    std::cout<<"value of __q__ after assigning it with shift,";
+    for(int j=0;j<4; j++){
+        std::cout<<__q__.at(j);
     }
 }
+void shift___a__(std::vector<char> & __a__){
+    __a__.clear(); /*because of the buffer*/
+    for (int i=0; i<(int)((__assign_all__.size()/2)-1); i++){
+        __a__.push_back(__assign_all__.at(i));
+    }
+    __a__.insert(__a__.begin(),__assign_all__.at(0));
+    std::cout<<"assigning of A after  pushing the values,: ";
+    for (int j=0; j<(int)(__a__.size()); j++){
+        std::cout<<__a__.at(j);
+    }
+}
+
+
+
+
+
+
+
+
+
 
