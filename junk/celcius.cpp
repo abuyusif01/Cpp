3b61ddb8d95f008957e6ced1cfcf5d8758654a60
blackdracula18 <Abubakaryusuf951@gmail.com>
2019-12-15 19:07:26 +0800

    cpp: create dir && organise stuff

---
+++ junk/celcius.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+    double c,f;
+    std::cout<<"enter the amount of energy \n";
+    std::cin>>c;
+    std::cout<<"the farenheit is :"<<(c*9/5)+32;
+    //just kinda learning git hmmm
+    //still lookint the changes and learning them all
+    return 0;
+}
+
---
+++ junk/quotient.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+int num,div;
+std::cout<<"enter the number space the divisor\n";
+std::cin>>num>>div;
+std::cout<<"here is the division of the number :"<<num/div<<"\n";
+std::cout<<"here is the modulus of the number :"<<num%div;
+return 0;
+}
