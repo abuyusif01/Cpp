e2cfa7f8a4ff39bbc973320c48723dfe85743bc4
blackdracula18 <Abubakaryusuf951@gmail.com>
2020-02-09 18:03:21 +0800

    Revert "Revert "Cpp: Still working On Clang-vim""
    This reverts commit c4dfbb038a9e1ddb181c89b82e8a9aa0b10f7a4b.

--- junk/celcius.cpp
+++ junk/celcius.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-void count() {
-    std::cout << "Nothing";
-}
-void line() {
-    std::cout << "Fuck";
-}
+void count() { std::cout << "Nothing"; }
+void line() { std::cout << "Fuck"; }
 void foo() {}
 int main() {
-    double c;
-    std::cout << "enter the amount of energy in Celcius: ";
-    std::cin >> c;
-    std::cout << "the farenheit is :" << (c * 9 / 5) + 32;
-    // just kinda learning git hmmm
-    // still lookint the changes and learning them all
-    return 0;
+  double c;
+  std::cout << "enter the amount of energy in Celcius: ";
+  std::cin >> c;
+  std::cout << "the farenheit is :" << (c * 9 / 5) + 32;
+  // just kinda learning git hmmm
+  // still lookint the changes and learning them all
+  return 0;
 }
 
