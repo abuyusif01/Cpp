843250ec8e3a3d7aef418156f28f8b997330498c
blackdracula18 <Abubakaryusuf951@gmail.com>
2021-05-10 17:06:53 +0800

    done with saddam assignment

--- junk/lines.c
+++ junk/lines.c
@@ -1,27 +1,65 @@
 #include <iostream>
-using std::cin;
-using std::cout;
-int main() {
-	int a, i, j = 1, x = 0, max;
-	cout<< "Input number of lines: ";
-	cin>>a >>max;
 
-	for(i = 1; i <= a; i++) {
-		while(x < max) {
-			
-			if ((j++)% 5 == 0 )
+using namespace std;
+
+int find_best(int x[], int y)
+{
+	int temp =x[0];
+	for (int i =0; i< y; i++)
+	{
+		if(x[i] > temp)
+		{
+			temp = x[i];
+		}
+	}
+	return temp;
+}
+
+int main()
+{
+	int n;
+	cout << "Enter the number of students: ";
+	cin >> n;
+	int* a= new int[n]; // allocating the array in heap
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	int best = find_best(a,n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (a[i] >= best - 10)
 			{
-				printf("%d ", j);
-			}
-			x++;
-			if (j > max)
-			{
+				cout << "Student " << i << " Score is " << a[i]
+					<< " and grade is A\n";
 				break;
 			}
+			else if (a[i] >= best - 20)
+			{
+				cout << "Student " << i << " Score is " << a[i]
+					<< " and grade is B\n";
+				break;
+			}
+			else if (a[i] >= best - 30)
+			{
+				cout << "Student " << i << " Score is " << a[i]
+					<< " and grade is C\n";
+				break;
+			}
+			else if (a[i] >= best - 40)
+			{
+				cout << "Student " << i << " Score is " << a[i]
+					<< " and grade is D\n";
+				break;
+			}
+			else 
+			{
+				cout<< "The grade is F";
+			}
 		}
-		x = 0;
-		cout<<"\n";
 	}
-	
 	return 0;
 }
